92c2737fbe86f9d4f16382da8a6b04b35f4a5acd
henryhoangduong <henryhoangduong@gmail.com>
2025-01-22 21:48:49 +0700

    bleu

---
+++ Transformers/util/bleu.py
@@ -0,0 +1,17 @@
+import math
+from collections import Counter
+
+import numpy as np
+
+
+def bleu_stats(hypothesis, reference):
+    pass
+
+def bleu(stats):
+    pass
+
+def get_bleu(hypothesis, reference):
+    pass
+
+def idx_to_word(x, vocab):
+    pass
